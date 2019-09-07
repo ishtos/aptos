522cbb03964a3dba61cf3c78cb36687b6eb29d93
ti-ginkgo <ginkgonut.9960@gmail.com>
2019-09-07 10:26:46 +0900

    add

--- py/fastai_main.py
+++ py/fastai_main.py
@@ -40,7 +40,7 @@ from fastai.vision import *
 from fastai.callbacks import *
 
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn.metrics import cohen_kappa_score
 
 import albumentations as A
@@ -199,28 +199,28 @@ def main():
         RandTransform(tfm=TfmAffine (zoom), kwargs={'scale': (1.0, 1.3)}, p=1.0, resolved={}, do_run=True, is_random=True, use_on_y=True),
     ])
 
-    old_data = (ImageList.from_df(df=old_df, path='./', cols='path')
-        .split_by_idx(list(range(idx_s, idx_e))) 
-        .label_from_df(cols='diagnosis', label_cls=FloatList)
-        .transform(tfms=tfms, size=size, resize_method=ResizeMethod.SQUISH, padding_mode='zeros') 
-        .databunch(bs=bs, num_workers=0) 
-        .normalize(imagenet_stats)  
-       )
+    # old_data = (ImageList.from_df(df=old_df, path='./', cols='path')
+    #     .split_by_idx(list(range(idx_s, idx_e))) 
+    #     .label_from_df(cols='diagnosis', label_cls=FloatList)
+    #     .transform(tfms=tfms, size=size, resize_method=ResizeMethod.SQUISH, padding_mode='zeros') 
+    #     .databunch(bs=bs, num_workers=0) 
+    #     .normalize(imagenet_stats)  
+    #    )
 
-    learn = Learner(data=old_data, 
-                model=model, 
-                path='../',
-                model_dir='weights',
-                metrics=[qk]).to_fp16()
+    # learn = Learner(data=old_data, 
+    #             model=model, 
+    #             path='../',
+    #             model_dir='weights',
+    #             metrics=[qk]).to_fp16()
 
-    learn.load(os.path.join('..', 'weights', 'stage-1-epoch-5-model-3'))
+    # learn.load(os.path.join('..', 'weights', 'stage-1-epoch-5-model-3'))
 
-    print("START OLD TRAIN")
+    # print("START OLD TRAIN")
 
-    learn.fit_one_cycle(1, 0.0005)
-    learn.save(os.path.join('stage-1-unfreeze-epoch-1-model-3'))
+    # learn.fit_one_cycle(1, 0.0005)
+    # learn.save(os.path.join('stage-1-unfreeze-epoch-1-model-3'))
 
-    print("END OLD TRAIN")
+    # print("END OLD TRAIN")
 
     data = (ImageList.from_df(df=df, path='./', cols='path') 
         .split_by_idx(valid_idx)
@@ -241,6 +241,7 @@ def main():
                                       folder='test_images',
                                       suffix='.png'))
 
+    learn.load('stage-1-unfreeze-epoch-1-model-3'))
     print("START TRAIN")
 
     skf = StratifiedKFold(n_splits=5)
